71ad191570fabb7858c690c4e81d3efb2b91a5e5
mrsanezhilan <165382737+mrsanezhilan@users.noreply.github.com>
2026-02-07 23:37:58 -0800

    Refactor MongoDB connection and set up server routes
    Updated MongoDB connection to use environment variable for URI and added basic server routes.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+import express from "express";
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://root:root@cluster0.10ro4xy.mongodb.net/?appName=Cluster0";
+const app = express();
+const port = process.env.PORT || 3000;
 
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.MONGO_URI);
 
 async function run() {
   try {
@@ -10,9 +12,15 @@ async function run() {
     console.log("✅ Connected to MongoDB!");
   } catch (err) {
     console.error("❌ Connection failed:", err);
-  } finally {
-    await client.close();
   }
 }
 
 run();
+
+app.get("/", (req, res) => {
+  res.send("Hello! Server is running.");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
