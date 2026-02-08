b374e63eef8b77acaf48eb21040823d4dba145f6
mrsanezhilan <165382737+mrsanezhilan@users.noreply.github.com>
2026-02-07 23:35:29 -0800

    Update MongoDB connection URI credentials

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://admin:admin@cluster0.kxf7fvs.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://root:root@cluster0.10ro4xy.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri);
 
